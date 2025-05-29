4b4124bc197ea8dab2b8e63c6ebf968d881997e3
JP Villalobos <jp.villalobos@cloudpipelines.it>
2025-05-29 05:50:52 +0000

    Deleted src

--- src/ui/streamlit_app.py
+++
@@ -1,9 +0,0 @@
-# streamlit_app.py
-import streamlit as st
-
-st.title("Buscador CFIA Inteligente")
-
-question = st.text_input("Haz tu pregunta:")
-if question:
-    response = final_chain.invoke(question)
-    st.write(response)
--- src/utils/geo.py
+++
@@ -1,4 +0,0 @@
-from geopy.geocoders import Nominatim
-geo = Nominatim(user_agent="my-cr-geocoder")
-loc = geo.geocode("Grecia, Alajuela, Costa Rica", country_codes="cr")
-print(loc.latitude, loc.longitude)
